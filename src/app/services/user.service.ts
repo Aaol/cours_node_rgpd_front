75bf4d6623df3f9519d47e7ce8148eade72200a2
Unknown <fubuke@hotmail.fr>
2018-03-08 13:42:09 +0100

    UserService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,8 +4,11 @@ import { environment } from '../../environments/environment';
 import { UserRegister } from '../Models/user-register';
 import { Observable } from 'rxjs/Observable';
 import { UserRegisterReponse } from '../Models/user-register-response';
+import {UserLogin} from "../Models/user-login";
 @Injectable()
 export class UserService {
+
+  public user: UserRegister;
 
   constructor(private httpClient: HttpClient) {
 
@@ -15,9 +18,16 @@ export class UserService {
     return this.httpClient.post<UserRegisterReponse>(environment.apiUrl + 'register', user);
   }
 
-  logIn() {
+  logIn(user: UserLogin) {
+    this.user = new UserRegister;
+    this.user.userName = user.login;
+    this.user.password = user.password;
+    this.user.email = 'unknown';
+  }
 
-  }
   logOff() {
+    this.user = new UserRegister;
   }
+
+
 }
